718e05bcac6ea766479f18680c0b742a9501d6a0
vamakin <vamakin@gmail.com>
2015-08-31 15:25:08 +0000

    skills rest api controller

---
+++ app/Http/Controllers/VerifiedSkillController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\VerifiedSkill;
+
+class VerifiedSkillController extends Controller
+{
+    /**
+     * Show the specified photo comment.
+     *
+     * @param  int  $photoId
+     * @param  int  $commentId
+     * @return Response
+     */
+    public function index()
+    {
+        return VerifiedSkill::whereNotNull('group_id')->get()->toArray();
+    }
+}
