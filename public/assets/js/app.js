54fc82b9b86e575f991aacd2b0bd99f88cbd9add
vamakin <vamakin@gmail.com>
2015-09-01 11:35:33 +0000

    add groups and skills collections to global scope

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,30 @@
-require( [ "skill-compass/views/main", "skill-compass/views/area", "bootstrap" ], function( MainView, AreaView ) { 
+require( [ 
+    "skill-compass/views/main",          
+    "skill-compass/views/area", 
+    "skill-compass/collections/groups",
+    "skill-compass/collections/skills",    
+    "bootstrap"
+         ], function( MainView, AreaView, Groups, Skills ) { 
     
-    var areaView = new AreaView();    
-    $("#area-content").append( areaView.render().$el );
+    window.app = {};
+    app.groups = new Groups();
+
+    app.skills = new Skills();
+
+    this.promisArr = [
+        app.groups.fetch(),
+        app.skills.fetch()
+    ];   
     
-    var mainView = new MainView();    
-    $("#main-content").append( mainView.render().$el );
+    var self = this;
+    $.when.apply($, this.promisArr).then( function() {
+        var areaView = new AreaView();    
+        $("#area-content").append( areaView.render().$el );
+
+        var mainView = new MainView();    
+        $("#main-content").append( mainView.render().$el );             
+    });
+    
+
     
 });
