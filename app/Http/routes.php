6028049cc46274561c6c93edee0a48e2973da676
vamakin <vamakin@gmail.com>
2015-09-16 17:52:59 +0000

    delete parse controller and rout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,8 +24,3 @@ Route::resource('api/skill', 'VerifiedSkillController');
 Route::post('api/popular-chart-info', 'ChartController@getPopularChartInfo');
 Route::post('api/related-chart-info', 'ChartController@getrelatedChartInfo');
 
-Route::get('/parse', "ParserController@parse");
-Route::get('/reattach', "ParserController@reattach");
-
-
-// Route::any('fakeapi/{all}', "JsonServerController@handleRequest")->where('all', '.*');
