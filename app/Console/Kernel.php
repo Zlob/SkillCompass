aa4e6bf826c2e57d6c2cd64e620133756fd5cbe0
vamakin <vamakin@gmail.com>
2015-09-16 17:49:21 +0000

    parsing and attaching sheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,10 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Skill;
+use App\Models\Job;
+use App\Helpers\HeadHunterParser;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,5 +30,29 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+        
+        $schedule->call(function () {
+            Log::info('attaching new verified skills started');
+            $skills = Skill::whereNotNull('verified_skill_id')->get();
+            foreach($skills as $skill){
+                $jobs = Job
+                    ::whereHas('skills', function ($query) use ($skill) {
+                        $query->where('skill_id', $skill->id);
+                    })
+                        ->whereHas('verifiedSkills', function ($query) use ($skill) {
+                            $query->where('verified_skill_id', $skill->verified_skill_id);
+                        }, '<', 1)
+                        ->get();
+                foreach($jobs as $job){
+                    $job->verifiedSkills()->attach($skill->verified_skill_id); 
+                }            
+            }
+        })->hourly();
+
+        $schedule->call(function () {
+            Log::info('HH parsing started');
+            $hhParser = new \App\Helpers\HeadHunterParser();
+            $hhParser->parse();          
+        })->daily();
     }
 }
