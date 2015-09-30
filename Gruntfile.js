287a9de87def5c1d32cb51097fbe326277318683
vamakin <vamakin@gmail.com>
2015-09-30 13:52:40 +0000

    add test task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,24 @@ module.exports = function(grunt) {
                 // Например: при установке "bootstrap" добавится две записи: "bootstrap" и "jquery"
                 transitive: true
             }
+        },
+        jasmine_node: {
+            options: {
+                forceExit: true,
+                match: '.',
+                matchall: false,
+                extensions: 'js',
+                specNameMatcher: 'spec'
+            },
+            all: ['.']
         }
     });
 
     // Загружаем задачу
     grunt.loadNpmTasks('grunt-bower-requirejs');
+    grunt.loadNpmTasks('grunt-jasmine-node');
     
     // Создаем другое имя задачи по которому мы будем ее вызывать
     grunt.registerTask('update-requirejs', ['bowerRequirejs']);    
+    grunt.registerTask('test', ['jasmine_node']);  
 }
