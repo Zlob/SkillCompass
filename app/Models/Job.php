8eb6109bac366458020d1a91d2b010bbf01e5f5c
vamakin <vamakin@gmail.com>
2015-10-03 13:09:48 +0000

    add getJobBySkills method

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Job extends Model {   
     
-    protected $fillable = ['url', 'cost', 'area_id', 'parse_date', 'begda', 'endda', 'vacancy_id'];    
+    protected $fillable = ['url', 'cost', 'name', 'area_id', 'parse_date', 'begda', 'endda', 'vacancy_id'];    
 
     public function skills()
     {
@@ -44,6 +44,120 @@ class Job extends Model {
         return $job;
     }
     
+
+    /**
+     * получает  массив вакансий за пол года, содержащих хотябы один навык из перечисленных
+     *
+     * @param $skillIds - массив навыков
+     * @param $areaId - регион
+     */  
+    public static function getJobWithSkills($skillIds, $areaId)
+    {
+        $currentDate = new \DateTime();
+        $halfYear = new \DateInterval('P3M');
+        $date = $currentDate->sub($halfYear)->format('Y-m-d');
+        $jobs = Job::with(['verifiedSkills' => function($query) use ($skillIds)
+                           {
+                               $query->whereNotIn('verified_skills.id', $skillIds);
+                           }
+                          ])
+            ->where('area_id', $areaId)
+            ->where('endda', '>=', $date)
+            ->whereHas('verifiedSkills', function ($query) use ($skillIds) 
+                       {
+                           $query->whereIn('verified_skills.id', $skillIds);
+                       }
+                      )
+            ->get();
+        
+        $groups = [];
+        foreach($jobs as $job){
+            $groups[$job->getGroupName($skillIds)][] = $job->toGroupElement();
+        }
+        $result = [];
+        foreach($groups as $groupName => $group){
+            $result[] = Job::getGroupAggregation($groupName, $group);
+        }
+        return $result;        
+    }
+    
+    private static function getGroupAggregation($groupName, $group)
+    {
+        usort($group, function($a, $b){
+            if ($a['cost'] === $b['cost']) {
+                return 0;
+            }
+            return ($a['cost'] > $b['cost']) ? 1 : -1;
+        });
+
+        $aggregation = [];
+        $aggregation['name'] = $groupName;
+        $aggregation['min'] = $group[0]['cost'];
+        $aggregation['max'] = end($group)['cost'];
+        $aggregation['mid'] = $group[floor((count($group)-1)/2)]['cost'];
+        $aggregation['total_count'] = count($group);
+        $active_jobs = array_filter($group, function($groupItem) {return $groupItem['actual'] === true; });
+        $aggregation['actual_count'] = count($active_jobs);
+        $result = [];
+        $result['aggregation'] = $aggregation;
+        $result['actual_jobs'] = $active_jobs;
+        return $result;
+    }
+    
+    /**
+     * формирует общее имя для группы навыков
+     *
+     * @param $excludeSkillIds - массив навыков
+     */  
+    public function getGroupName($excludeSkillIds)
+    {
+        return $this->verifiedSkills
+            ->sort(function($a, $b)
+                   {
+                       if ($a->id === $b->id) {
+                           return 0;
+                       }
+                       return ($a->id > $b->id) ? 1 : -1;
+                   }
+                  )
+            ->reduce( function($carry, $skill) 
+                     {
+                         return $carry === "" ? $skill['name'] : $carry." ".$skill['name'];
+                     }, "");
+    }
+    
+    /**
+     * приводит вакансию к нужному массиву
+     *
+     */ 
+    public function toGroupElement()
+    {
+        return [
+            'id'     => $this->id,
+            'cost'   => $this->cost,
+            'url'    => $this->url,
+            'actual' => $this->getActual(),  
+            'additional_skills_count' => $this->verifiedSkills->count()
+        ]; 
+    }
+    
+    /**
+     * проверяет, является ли вакансия актуальной
+     *
+     */
+    private function getActual()
+    {
+        $currentDate = new \DateTime();
+        $threeDays = new \DateInterval('P3D');
+        $date = $currentDate->sub($threeDays)->format('Y-m-d');
+        if($this->endda >= $date){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }       
+    
     /**
      * Связывает вакансию с навыками
      *
@@ -71,10 +185,7 @@ class Job extends Model {
     {
         $this->endda = date('Y-m-d');
         $this->save();
-    }  
-    
-    
-
+    }
 }
 
 ?>
