e81c2e9821802d3af0796654442e146e90866fa6
vamakin <vamakin@gmail.com>
2015-10-03 13:05:57 +0000

    add name column

---
+++ database/migrations/2015_10_03_111628_add_name_to_job.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNameToJob extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jobs', function ($table) {
+            $table->string('name', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jobs', function ($table) {
+            $table->dropColumn('name');
+        });
+    }
+}
